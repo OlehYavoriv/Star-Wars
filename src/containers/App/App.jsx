442ccfb7625dc3f69fe4b60da006584fd435b560
Oleh Yavoriv <oleh.yavoriv@gmail.com>
2022-06-20 18:11:30 +0300

    Create and add a base

---
+++ src/containers/App/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import PeoplePage from "../PeoplePage";
+// import styles from "./App.module.scss";
+
+const App = () => {
+  return (
+    <>
+      {" "}
+      <PeoplePage />
+    </>
+  );
+};
+
+export default App;
