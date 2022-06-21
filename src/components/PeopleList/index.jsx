c0e76f29786c233fb78a0307144334f52adce7a5
Oleh Yavoriv <oleh.yavoriv@gmail.com>
2022-06-21 10:56:34 +0300

    Create PeopleList component

---
+++ src/components/PeopleList/index.jsx
@@ -0,0 +1,18 @@
+import styles from "./styles.module.scss";
+
+const PeopleList = ({ people }) => {
+  return (
+    <ul className={styles.list__container}>
+      {people.map(({ id, name, img }) => (
+        <li className={styles.list__item} key={id}>
+          <a href="#">
+            <img className={styles.person__photo} src={img} alt={name} />
+            <p>{name}</p>
+          </a>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default PeopleList;
