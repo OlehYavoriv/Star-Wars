58d8ae23783213e5c5168c13dc19f2ba6ee67ab2
Oleh Yavoriv <oleh.yavoriv@gmail.com>
2022-06-21 10:57:30 +0300

    Update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./styles/index.scss";
 import App from "./containers/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
