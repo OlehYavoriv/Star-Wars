c827a8c18bad3977fa89264a648fde3e9e5bd72a
Oleh Yavoriv <oleh.yavoriv@gmail.com>
2022-06-21 23:56:23 +0300

    Create error HOC

---
+++ src/hoc/withErrorApi.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import ErrorMessage from "../components/ErrorMessage";
+
+export const withErrorApi = (View) => {
+  return (props) => {
+    const [errorApi, setErrorApi] = useState(false);
+
+    return (
+      <>
+        {errorApi ? (
+          <ErrorMessage />
+        ) : (
+          <View setErrorApi={setErrorApi} {...props} />
+        )}
+      </>
+    );
+  };
+};
+
+export default withErrorApi;
