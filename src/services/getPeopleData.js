ae76c21a236ca44cbd9558aac4a66aa678116abd
Oleh Yavoriv <oleh.yavoriv@gmail.com>
2022-06-21 10:56:50 +0300

    Add new data

---
+++ src/services/getPeopleData.js
@@ -0,0 +1,31 @@
+import {
+  HTTP,
+  HTTPS,
+  SWAPI_ROOT,
+  SWAPI_PEOPLE,
+  GUIDE_IMG_EXTENSION,
+  URL_IMG_PERSON,
+} from "../constants/api";
+
+const checkProtocol = (url) => {
+  if (url.indexOf(HTTPS) !== -1) {
+    return HTTPS;
+  }
+
+  return HTTP;
+};
+
+const getId = (url, category) => {
+  const protocol = checkProtocol(url);
+
+  const id = url
+    .replace(protocol + SWAPI_ROOT + category, "")
+    .replace(/\//g, "");
+
+  return id;
+};
+
+export const getPeopleId = (url) => getId(url, SWAPI_PEOPLE);
+
+export const getPeopleImage = (id) =>
+  `${URL_IMG_PERSON}/${id + GUIDE_IMG_EXTENSION}`;
