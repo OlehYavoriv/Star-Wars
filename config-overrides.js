26faea2bdeb23b685e54fc664c8c1e7564edd106
Oleh Yavoriv <oleh.yavoriv@gmail.com>
2022-06-22 08:56:22 +0300

    Create config for alias

---
+++ config-overrides.js
@@ -0,0 +1,15 @@
+const { alias } = require("react-app-rewire-alias");
+
+module.exports = function override(config, env) {
+  alias({
+    "@assets": "src/assets",
+    "@components": "src/components",
+    "@constants": "src/constants",
+    "@containers": "src/containers",
+    "@hoc": "src/hoc",
+    "@services": "src/services",
+    "@styles": "src/styles",
+    "@utils": "src/utils",
+  })(config);
+  return config;
+};
