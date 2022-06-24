b991d3364c2ee4d2f12ae8d5e88c96c144ad0b41
Oleh Yavoriv <oleh.yavoriv@gmail.com>
2022-06-24 23:32:12 +0300

    Refactoring code and update

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import App from "@containers/App";
 import "./styles/index.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
