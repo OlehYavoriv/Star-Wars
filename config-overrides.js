9da3517fbc09dd026f69b030c813b594d51f39e1
Oleh Yavoriv <oleh.yavoriv@gmail.com>
2022-07-01 12:59:54 +0300

    Add minor changes

--- config-overrides.js
+++ config-overrides.js
@@ -4,6 +4,7 @@ module.exports = function override(config, env) {
   alias({
     "@assets": "src/assets",
     "@components": "src/components",
+    "@ui": "src/components/Ui-Kit",
     "@constants": "src/constants",
     "@containers": "src/containers",
     "@hoc": "src/hoc",
